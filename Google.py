22160c32e0f028fb47917236558f6cc1f1d8d31c
Ahmed Rafsan Raqib <46869748+rafsanraqib@users.noreply.github.com>
2019-02-03 11:42:42 -0600

    Update Google.py

--- Google.py
+++ Google.py
@@ -5,10 +5,10 @@ from bs4 import BeautifulSoup
 def meaning(n):
     url = "https://www.urbandictionary.com/define.php?term="+ n
     resp = requests.get(url)
-    parser = BeautifulSoup(resp.text, 'html.parser')
-    clss = parser.find("div", {"class" : "meaning"})
-    for i in clss.findAll("a"):
-        print(i.text)
+    parser = BeautifulSoup(resp.text, 'html.parser') #parses the HTML file
+    clss = parser.find("div", {"class" : "meaning"}) #looks for the class meaning in the HTML code
+    for i in clss.findAll("a"):                      #Finds all anchor elements in the HTML code
+        print(i.text)                                #Prints all the text corresponding the meaning class
 while True:
     UserInput = input()
     if UserInput == "quit":
